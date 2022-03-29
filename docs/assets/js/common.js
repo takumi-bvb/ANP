f8d212ed3c3fb7248eb24436996748d9693083f9
sugimoto <takumi.psr@gmail.com>
2022-03-29 15:21:29 +0900

    common.jsのファイル名をrollupに合わせて修正

--- docs/assets/js/common.js
+++ docs/assets/js/common.js
@@ -1,11 +1,17 @@
 (function () {
   'use strict';
 
-  var toggleDrawer = function toggleDrawer() {
-    document.body.classList.toggle('is-drawer-active');
-  };
+  (d => {
+    const toggleDrawer = () => {
+      d.body.classList.toggle('is-drawer-active');
+    };
 
-  document.getElementById('drawer_btn').addEventListener('click', toggleDrawer);
-  document.getElementById('drawer_shade').addEventListener('click', toggleDrawer);
+    d.getElementById('drawer_btn').addEventListener('click', toggleDrawer);
+    d.getElementById('drawer_shade').addEventListener('click', toggleDrawer);
+  })(document);
+
+  console.log("saa");
+  // import './modules/slider'
+  // import './modules/scroll'
 
 })();
